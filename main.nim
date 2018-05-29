9f8a1e29b9176cb6543960cc7fbdd7532faeeee9
Anirudh <icyph0x@pm.me>
2018-05-29 16:45:31 +0530

    feat: format repo name into output

--- main.nim
+++ main.nim
@@ -1,11 +1,13 @@
 import os
 import ospaths
+import strformat
 
 proc init(repo: string) =
   os.createDir(ospaths.joinPath(repo, ".git"))
   for name in ["objects", "refs", "refs/heads"]:
     os.createDir(ospaths.joinPath(repo, ".git", name))
   writeFile(ospaths.joinPath(repo, ".git", "HEAD"), "ref: refs/heads/master")
+  echo fmt"initialized empty repository: {repo}"
 
 
 proc main() =
