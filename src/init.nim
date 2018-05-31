776d9153b775303f4878747925ca4ba6ff3b60d2
Anirudh <icyph0x@pm.me>
2018-05-31 11:30:08 +0530

    fix(init): functions are already in scope

--- src/init.nim
+++ src/init.nim
@@ -1,25 +1,28 @@
+# init.nim: initializes a git repository
+
 import os
 import ospaths
 import strformat
 
-const GIT_DIR = ".git"
-const OBJECTS_DIR = ospaths.joinPath(GIT_DIR, "objects")
-const REFS_DIR = ospaths.joinPath(GIT_DIR, "refs")
+const
+  GIT_DIR = ".git"
+  OBJECTS_DIR = joinPath(GIT_DIR, "objects")
+  REFS_DIR = joinPath(GIT_DIR, "refs")
 
 proc create_objects_dir() =
-  os.createDir(OBJECTS_DIR)
-  os.createDir(ospaths.joinPath(OBJECTS_DIR, "info"))
-  os.createDir(ospaths.joinPath(OBJECTS_DIR, "pack"))
+  createDir(OBJECTS_DIR)
+  createDir(joinPath(OBJECTS_DIR, "info"))
+  createDir(joinPath(OBJECTS_DIR, "pack"))
 
 proc create_refs_dir() =
-  os.createDir(REFS_DIR)
-  os.createDir(ospaths.joinPath(REFS_DIR, "head"))
-  os.createDir(ospaths.joinPath(REFS_DIR, "tags"))
+  createDir(REFS_DIR)
+  createDir(joinPath(REFS_DIR, "head"))
+  createDir(joinPath(REFS_DIR, "tags"))
 
 proc initialize_head() =
-  writeFile(ospaths.joinPath(GIT_DIR, "HEAD"), "ref: refs/heads/master")
+  writeFile(joinPath(GIT_DIR, "HEAD"), "ref: refs/heads/master")
 
-os.createDir(GIT_DIR)
+createDir(GIT_DIR)
 create_objects_dir()
 create_refs_dir()
 initialize_head()
